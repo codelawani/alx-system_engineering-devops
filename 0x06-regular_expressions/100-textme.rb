918b82fe5d4fff818b97bed711d4996cf98514ff
Nico <lawani.gk@gmail.com>
2023-01-31 16:01:06 +0100

    fix line 3

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 # puts ARGV[0].scan(/\[from:(\w+|\+\d{11})\] \[to:(\+?\d{11})\] \[flags:([-10:]+)/).join(',')
-puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)/).join(',')
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(',')
