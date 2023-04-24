dfa6bd5faf0984378d4804ef6708ddadc49c27f6
Nico <lawani.gk@gmail.com>
2023-04-24 14:44:43 +0100

    modify files

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 """This script exports data retrieved
 from an api request in JSON format"""
-import json
-import requests as rq
-from sys import argv
+if __name__ == '__main__':
+    import json
+    import requests as rq
+    from sys import argv
 
-user_id = argv[1] if argv[1:] else 1
-FILENAME = user_id + ".json"
+    user_id = argv[1] if argv[1:] else 1
+    FILENAME = user_id + ".json"
 
-url = 'https://jsonplaceholder.typicode.com/'
-user = rq.get(url + 'users/{}'.format(user_id)).json()
-tasks = rq.get(url + 'users/{}/todos'.format(user_id)).json()
-data = {user_id: [{'task': task.get('title'),
-                   'completed': task.get('completed'),
-                   'username': user.get('name')} for task in tasks]}
-with open(FILENAME, 'w', newline='') as f:
-    json.dump(data, f)
+    url = 'https://jsonplaceholder.typicode.com/'
+    user = rq.get(url + 'users/{}'.format(user_id)).json()
+    tasks = rq.get(url + 'users/{}/todos'.format(user_id)).json()
+    data = {user_id: [{'task': task.get('title'),
+                       'completed': task.get('completed'),
+                       'username': user.get('name')} for task in tasks]}
+    with open(FILENAME, 'w', newline='') as f:
+        json.dump(data, f)
--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -2,20 +2,22 @@
 """This script exports data retrieved
 from an api request in JSON format"""
 
-import json
-import requests as rq
-from sys import argv
+if __name__ == '__main__':
+    import json
+    import requests as rq
+    from sys import argv
 
-FILENAME = "todo_all_employees.json"
+    FILENAME = "todo_all_employees.json"
 
-url = 'https://jsonplaceholder.typicode.com/'
-users = rq.get(url + 'users').json()
-tasks = rq.get(url + 'todos').json()
-data = {user.get('id'): [{'username': user.get('name'),
-                          'task': task.get('title'),
-                          'completed': task.get('completed'),
-                          }
-        for task in tasks if task.get('userId') == user.get('id')]
-        for user in users}
-with open(FILENAME, 'w', newline='') as f:
-    json.dump(data, f)
+    url = 'https://jsonplaceholder.typicode.com/'
+    users = rq.get(url + 'users').json()
+    tasks = rq.get(url + 'todos').json()
+    data = {user.get('id'): [{'username': user.get('name'),
+                              'task': task.get('title'),
+                              'completed': task.get('completed'),
+                              }
+            for task in tasks if task.get('userId') == user.get('id')]
+            for user in users}
+    with open(FILENAME, 'w', newline='') as f:
+        json.dump(data, f)
+
