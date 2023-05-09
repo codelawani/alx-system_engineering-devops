faa58418a00891dcaa2f24db69fff0ab8f814a65
Nico <lawani.gk@gmail.com>
2023-05-09 19:52:48 +0100

    add script that gets subreddit hot 10 posts

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This script queries the Reddit API
+and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit"""
+import requests
+
+
+def top_ten(subreddit):
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    response = requests.get(url, allow_redirects=False, headers={
+                            "User-Agent": "Nico@alx/1.0"})
+    if response.status_code != 200:
+        return None
+    else:
+        data = response.json()
+        for child in data['data']['children']:
+            print(child['data']['title'])
